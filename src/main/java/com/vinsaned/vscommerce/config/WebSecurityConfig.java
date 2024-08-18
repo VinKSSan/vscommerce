07f33cbc1e0b91a3733401c64d9bddca4d972a1f
VinKSSan <vinsan.ed@proton.me>
2024-08-18 15:53:31 -0300

    Registrando componente Bcrypt para encodarmos senhas

--- src/main/java/com/vinsaned/vscommerce/config/WebSecurityConfig.java
+++ src/main/java/com/vinsaned/vscommerce/config/WebSecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 //para configurarmos componentes do sistema
 @Configuration
 public class WebSecurityConfig {
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
