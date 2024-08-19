4a3f790d6c15f6e58621b4c67c9be6ad53a52fbf
VinKSSan <vinsan.ed@proton.me>
2024-08-18 21:06:33 -0300

    Oauth2 jwt checkList, configurando webSecurity

--- src/main/java/com/vinsaned/vscommerce/config/WebSecurityConfig.java
+++ src/main/java/com/vinsaned/vscommerce/config/WebSecurityConfig.java
@@ -3,64 +3,39 @@ package com.vinsaned.vscommerce.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-//para configurarmos componentes do sistema
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
     @Value("${jwt.secret}")
     private String jwtSecret;
 
     @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder(){
+    BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest()
-                        .authenticated()
-                )
-                .oauth2ResourceServer(outh2->outh2
-                        .jwt(jwt->jwt
-                                .jwtAuthenticationConverter(jac())
-                        )
-                )
-        ;
-        //http.headers(h->h.frameOptions(f->f.disable()));
-        //http.csrf(AbstractHttpConfigurer::disable);
-        return  http.build();
+    JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(jwtSecret);
+        return tokenConverter;
     }
+
     @Bean
-    public JwtDecoder jwtDecoder(){
-        SecretKey sk = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
-        return NimbusJwtDecoder.withSecretKey(sk).build();
+    JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    private JwtAuthenticationConverter jac() {
-        return new JwtAuthenticationConverter();
     }
 }
