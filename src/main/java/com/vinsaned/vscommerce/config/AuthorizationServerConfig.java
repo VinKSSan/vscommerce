f0b8e96fde5133c77ac8354242ca0ca4153cdf34
VinKSSan <vinsan.ed@proton.me>
2024-08-18 21:07:04 -0300

    Oauth2 jwt checkList, configurando servidor de autorização

--- src/main/java/com/vinsaned/vscommerce/config/AuthorizationServerConfig.java
+++ src/main/java/com/vinsaned/vscommerce/config/AuthorizationServerConfig.java
@@ -1,29 +1,63 @@
+package com.vinsaned.vscommerce.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
 @Configuration
-@EnableWebSecurity
-public class AuthorizationServerConfig {
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Bean
-    public OAuth2AuthorizationServerSettings authorizationServerSettings() {
-        return OAuth2AuthorizationServerSettings.builder()
-                .issuer("http://localhost:8080")
-                .build();
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    @Value("${jwt.duration}")
+    private Integer jwtDuration;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
-    @Bean
-    public InMemoryRegisteredClientRepository registeredClientRepository() {
-        // Configure your registered clients here
-        return new InMemoryRegisteredClientRepository(/* Your registered clients */);
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient(clientId)
+                .secret(passwordEncoder.encode(clientSecret))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(jwtDuration);
     }
 
-    @Bean
-    public OAuth2TokenGenerator<?> tokenGenerator() {
-        return new DefaultOAuth2TokenGenerator();
-    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
-    @Bean
-    public OAuth2AuthorizationService authorizationService() {
-        return new OAuth2AuthorizationService();
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter);
     }
-
-    // Define any additional beans required for your configuration
 }
