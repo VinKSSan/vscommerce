349521b0039a4a0793e053aac624021bea0b5ae5
VinKSSan <vinsan.ed@proton.me>
2024-08-22 22:20:26 -0300

    Criando serciço do usuário // Creating service from users

--- src/main/java/com/vinsaned/vscommerce/services/UserService.java
+++ src/main/java/com/vinsaned/vscommerce/services/UserService.java
@@ -1,12 +1,16 @@
 package com.vinsaned.vscommerce.services;
 
+import com.vinsaned.vscommerce.dto.ProductDTO;
+import com.vinsaned.vscommerce.dto.UserDTO;
 import com.vinsaned.vscommerce.entities.User;
 import com.vinsaned.vscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -20,5 +24,20 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException("Email not found");
         }
         return user;
+    }
+
+    protected User authenticated(){
+        try{
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return repo.findByEmail(username);
+        }catch (Exception e){
+            throw  new UsernameNotFoundException("Invalid user");
+        }
+    }
+
+    @Transactional
+    public UserDTO getMe() {
+        User entity = authenticated();
+        return new UserDTO(entity);
     }
 }
