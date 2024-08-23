5be55229dff964eabd9499b64e133a201af46eba
VinKSSan <vinsan.ed@proton.me>
2024-08-22 22:18:59 -0300

    Criando DTO do usuário // Created DTO from users

---
+++ src/main/java/com/vinsaned/vscommerce/dto/UserDTO.java
@@ -0,0 +1,59 @@
+package com.vinsaned.vscommerce.dto;
+
+import com.vinsaned.vscommerce.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO(long id, String name, String email, String phone, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        phone = entity.getPhone();
+        birthDate = entity.getBirthDate();
+        for (GrantedAuthority role: entity.getAuthorities()){
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
