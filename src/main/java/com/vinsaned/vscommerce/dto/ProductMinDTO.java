e2f7b3cc41a1e73b285a1f98d84f6ca8db104291
VinKSSan <vinsan.ed@proton.me>
2024-08-22 23:24:18 -0300

    criando exibição minima dos produtos / creating minimal product display

---
+++ src/main/java/com/vinsaned/vscommerce/dto/ProductMinDTO.java
@@ -0,0 +1,45 @@
+package com.vinsaned.vscommerce.dto;
+
+import com.vinsaned.vscommerce.entities.Product;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+public class ProductMinDTO {
+    private Long id;
+    private String name;
+    private Double price;
+    private String imgUrl;
+
+    public ProductMinDTO(){}
+
+    public ProductMinDTO(Long id, String name, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+    public ProductMinDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
